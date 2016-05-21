17e82e2e974d43f22515c0418a235ddd43695891
ZhuangYu <bbzz7@163.com>
2016-05-21 21:17:14 +0800

    three bug merger

--- day 3.js
+++ day 3.js
@@ -1,9 +1,4 @@
 /**
  * Created by frog on 16/5/21.
  */
-<<<<<<< HEAD
-console.log('this is day 3, fix a bug,,and two bug,I'm master);
-=======
-console.log('this is day 3, fix three bug,I'm master);
-
->>>>>>> bug
+console.log("this is day 3, fix three bug,I'm master");
